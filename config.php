717605772b2e264f764305a24fe47e09b7c73973
Lucas Burlingham <lucas@lucasburlingham.me>
2022-11-13 16:35:20 -0500

    Added sample values and remove header

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
 
-
-// Configuration file; To be included on all pages with:
-// require_once("/path/to/config.php");
-
-
 // Application variables
 $extension_dir = "/usr/share/google-chrome/extensions/";
 $extension_ver= "0.0.1";
-$extension_ID = "";
+$extension_ID = "aaaaaaaaaaaaaaabbbbbbbbbcccccc";
 
 // Backend variables
-$db_path = "";
+$db_path = "titles.db";
 $crx_path = "";
 
 
